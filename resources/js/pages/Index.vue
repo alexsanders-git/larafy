d90c204788c082124d924700e5d119af8121a319
alex <alex.sanders@ukr.net>
2025-03-30 20:14:44 +0300

    Clear started project

---
+++ resources/js/pages/Index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Home"/>
+    <h1>Hello Laravel!</h1>
+    <Link :href="route('show')">Show Page</Link>
+</template>
