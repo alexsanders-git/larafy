a64587c3fbcdf0deed41d2b37cdad4f7044f18b2
alex <alex.sanders@ukr.net>
2025-04-06 18:23:25 +0300

    Listings: Use Local Scopes for filters

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -15,30 +15,11 @@ class ListingController extends Controller {
      */
     public function index( Request $request ) {
         $filters = $request->only( [ 'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo', ] );
-        $query = Listing::orderByDesc( 'created_at' )
-            ->when(
-                $filters[ 'priceFrom' ] ?? false,
-                fn( $query, $value ) => $query->where( 'price', '>=', $value )
-            )->when(
-                $filters[ 'priceTo' ] ?? false,
-                fn( $query, $value ) => $query->where( 'price', '<=', $value )
-            )->when(
-                $filters[ 'beds' ] ?? false,
-                fn( $query, $value ) => $query->where( 'beds', (int)$value < 6 ? '=' : '>=', $value )
-            )->when(
-                $filters[ 'baths' ] ?? false,
-                fn( $query, $value ) => $query->where( 'baths', (int)$value < 6 ? '=' : '>=', $value )
-            )->when(
-                $filters[ 'areaFrom' ] ?? false,
-                fn( $query, $value ) => $query->where( 'area', '>=', $value )
-            )->when(
-                $filters[ 'areaTo' ] ?? false,
-                fn( $query, $value ) => $query->where( 'area', '<=', $value )
-            );
 
         return Inertia::render( 'listing/Index', [
             'filters' => $filters,
-            'listings' => $query
+            'listings' => Listing::mostRecent()
+                ->filter( $filters )
                 ->paginate( 9 )
                 ->withQueryString()
         ] );
